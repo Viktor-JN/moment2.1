bad9fa522866327824eb8c7f94ee96a15b612c52
Viktor-JN <viktor.johanssonnygren@elev.ntig.se>
2023-12-06 11:34:14 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Hello world");
+
+function showPopup(){
+    alert("Hej hej");
+}
+
+function changeTag(){
+    document.getElementById("changeText").innerHTML = "";
+    document.getElementById("button-change").innerHTML = "gone";
+}
